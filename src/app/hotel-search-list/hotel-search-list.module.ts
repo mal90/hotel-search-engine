4a84d9c5ecc035c77c065ecef13c0e0714972d98
Malik <madkonline@gmail.com>
2018-12-10 19:56:59 +0530

    Results view page implementation

--- src/app/hotel-search-list/hotel-search-list.module.ts
+++ src/app/hotel-search-list/hotel-search-list.module.ts
@@ -2,20 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HotelSearchComponent } from './hotel-search/hotel-search.component';
 import { SearchListComponent } from './search-list/search-list.component';
+import { ShortDescription } from './short-description.pipe';
 import { HotelSearchService } from './hote-search-list.service';
-import { NgSelectModule } from '@ng-select/ng-select';
 import { FormsModule } from '@angular/forms';
 
+
 @NgModule({
-  declarations: [HotelSearchComponent, SearchListComponent],
+  declarations: [HotelSearchComponent, SearchListComponent, ShortDescription],
   imports: [
     CommonModule,
-    NgSelectModule,
     FormsModule
   ],
   exports : [
     HotelSearchComponent,
-    SearchListComponent
+    SearchListComponent,
+    ShortDescription
   ],
   providers : [
     HotelSearchService
---
+++ src/app/hotel-search-list/short-description.pipe.ts
@@ -0,0 +1,8 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({name: 'shortDescription'})
+export class ShortDescription implements PipeTransform {
+  transform(value: string): string {
+    return value === undefined || value === ""  ? "" : value.substring(0, 55).concat("...");
+  }
+}
