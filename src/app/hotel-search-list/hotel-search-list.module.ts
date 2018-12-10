a8a7b24156f7f77cb4338fba384c8f9ad8174247
Malik <madkonline@gmail.com>
2018-12-10 20:54:55 +0530

    Replace ngx bootstrap with ngb bootsrap , changes done to results page

--- src/app/hotel-search-list/hotel-search-list.module.ts
+++ src/app/hotel-search-list/hotel-search-list.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HotelSearchComponent } from './hotel-search/hotel-search.component';
+import { NgbRatingModule , NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 import { SearchListComponent } from './search-list/search-list.component';
 import { ShortDescription } from './short-description.pipe';
 import { HotelSearchService } from './hote-search-list.service';
@@ -11,7 +12,9 @@ import { FormsModule } from '@angular/forms';
   declarations: [HotelSearchComponent, SearchListComponent, ShortDescription],
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    NgbRatingModule,
+    NgbDropdownModule
   ],
   exports : [
     HotelSearchComponent,
--- src/app/hotel-search-list/hotel-search/hotel-search.component.ts
+++ src/app/hotel-search-list/hotel-search/hotel-search.component.ts
@@ -9,6 +9,10 @@ import { HotelSearchService } from '../hote-search-list.service';
 export class HotelSearchComponent implements OnInit {
 
   hotelList:any[] = [];
+  maxRate:number = 10; // max rating of the hotel is set to default value of = 10
+  isRatingReadonly: boolean = true; // max rating is bydefault readonly
+  currencyTypes:string[] = ["USD","SGD","CNY","KRW"];
+
 
   constructor(private hotelSearch : HotelSearchService ) { }
 
