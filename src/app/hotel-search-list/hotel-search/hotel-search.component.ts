3561f648f4245cda589c7b553e79a1a44c7b42c8
Malik <madkonline@gmail.com>
2018-12-12 21:18:13 +0530

    Save selected currency in the localstorage

--- src/app/hotel-search-list/hotel-search/hotel-search.component.ts
+++ src/app/hotel-search-list/hotel-search/hotel-search.component.ts
@@ -12,12 +12,14 @@ export class HotelSearchComponent implements OnInit {
   maxRate:number = 10; // max rating of the hotel is set to default value of = 10
   isRatingReadonly: boolean = true; // max rating is bydefault readonly
   currencyTypes:string[] = ["USD","SGD","CNY","KRW"];
-  selectedCurrency = this.currencyTypes[0];
+  selectedCurrency:string;
 
 
   constructor(private hotelSearchService : HotelSearchService ) { }
 
   ngOnInit() {
+    let savedCurrency = localStorage.getItem('currency');
+    this.selectedCurrency = savedCurrency ?  savedCurrency : this.currencyTypes[0];
     let currentHotel = "tokyo";
     this.hotelSearchService.getHotel(currentHotel).subscribe(hotelList => {
       console.log(hotelList);
@@ -26,13 +28,15 @@ export class HotelSearchComponent implements OnInit {
     });
   }
 
-  changeCurrency(newCurrency: string) { 
+  changeCurrency(newCurrency: string) {
+    localStorage.setItem('currency', newCurrency);
     this.selectedCurrency = newCurrency;
     this.hotelSearchService.getPriceByCurrency(newCurrency).subscribe(priceList => {
       console.log(priceList);
       this.hotelList.forEach(hotel => {
         hotel.currency = newCurrency;
-        hotel.price = priceList.filter(price => price.id == hotel.id)[0].price;
+        let selectedPrice = priceList.filter(price => price.id == hotel.id)[0];
+        hotel.price = selectedPrice !== undefined ? selectedPrice.price : "rate is unavailable" ;
       });
     });
   }
