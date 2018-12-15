7cc181f5db2bb326a4721dcfe5798c0fc373b854
Malik <madkonline@gmail.com>
2018-12-15 08:07:30 +0530

    Add model interfaces, refactor code

--- src/app/hotel-search-list/hotel-list/hotel-list.component.ts
+++ src/app/hotel-search-list/hotel-list/hotel-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { HotelSearchService } from '../shared/hotel-search-list.service';
+import { HotelSearchService } from '../shared/hotel-list.service';
+import { Hotel, Price } from '../shared/hotel-list.model';
 
 @Component({
   selector: 'app-hotel-list',
@@ -8,18 +9,18 @@ import { HotelSearchService } from '../shared/hotel-search-list.service';
 })
 export class HotelListComponent implements OnInit {
 
-  hotelList:any[] = [];
-  maxRate:number = 10; // max rating of the hotel is set to default value of = 10
+  hotelList: Hotel[] = [];
+  maxRate: number = 10; // max rating of the hotel is set to default value of = 10
   isRatingReadonly: boolean = true; // max rating is bydefault readonly
-  currencyTypes:string[] = ["USD","SGD","CNY","KRW"];
-  selectedCurrency:string;
+  currencyTypes: string[] = ["USD", "SGD", "CNY", "KRW"];
+  selectedCurrency: string;
 
 
-  constructor(private hotelSearchService : HotelSearchService ) { }
+  constructor(private hotelSearchService: HotelSearchService) { }
 
   ngOnInit() {
     let savedCurrency = localStorage.getItem('currency');
-    this.selectedCurrency = savedCurrency ?  savedCurrency : this.currencyTypes[0];
+    this.selectedCurrency = savedCurrency ? savedCurrency : this.currencyTypes[0];
     let currentHotel = "tokyo";
     this.hotelSearchService.getHotel(currentHotel).subscribe(hotelList => {
       console.log(hotelList);
@@ -29,23 +30,26 @@ export class HotelListComponent implements OnInit {
   }
 
   changeCurrency(newCurrency: string) {
-    localStorage.setItem('currency', newCurrency);
-    this.selectedCurrency = newCurrency;
     this.hotelSearchService.getPriceByCurrency(newCurrency).subscribe(priceList => {
       console.log(priceList);
-      this.hotelList.forEach(hotel => {
-        hotel.currency = newCurrency;
-        let selectedPrice = priceList.filter(price => price.id == hotel.id)[0];
-        if(selectedPrice){
-          hotel.price = selectedPrice.price;
-          hotel.currency = newCurrency;
-        }
-      });
+      this.refreshResults(priceList, newCurrency);
     });
   }
 
-  sortHotelResults(){
-    //TO DO
+  refreshResults(priceList:Price[], newCurrency:string) {
+    console.log(priceList);
+    this.hotelList.forEach(hotel => {
+      hotel.currency = newCurrency;
+      let selectedPrice = priceList.filter(price => price.id == hotel.id)[0];
+      if (selectedPrice) {
+        hotel.price = selectedPrice.price;
+        hotel.currency = newCurrency;
+      }
+      else {
+        hotel.price = undefined;
+        hotel.currency = undefined;
+      }
+    });
   }
 
 }
---
+++ src/app/hotel-search-list/shared/hotel-list.model.ts
@@ -0,0 +1,16 @@
+export interface Hotel {
+    id:number;
+    name:string;
+    rating:number;
+    starts:number;
+    address:string;
+    photo:string;
+    description:string;
+    currency:string;
+    price:number;
+}
+
+export interface Price{
+    id:number;
+    price:number;
+}
